005e50ae0683450fd59cbe06c42adf634a6af029
Pankajghz <135798617+Pankajghz@users.noreply.github.com>
2024-09-02 00:15:25 +0530

    Update Hero.jsx

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="flex flex-col-reverse md:flex-row items-center md:justify-between mb-12 px-6 lg:px-16 xl:px-24 md:pt-12">
       <div className="md:w-7/12 pb-16 items-center">
         <h1 className="text-3xl md:text-5xl xl:text-6xl font-bold text-purple-800 mb-6 leading-tight md:leading-tight xl:leading-snug">
-          Learn Upgrade and Upskill to Get Ahead in Your Career
+          Learn, Upgrade, and Upskill to Get Ahead in Your Career
         </h1>
         <p className="text-sm md:text-lg xl:text-xl text-gray-700 leading-relaxed mb-8">
           Explore our comprehensive programs designed to elevate your skills and
